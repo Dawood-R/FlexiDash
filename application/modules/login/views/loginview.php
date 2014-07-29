b25fd201dee50b1bd9bfcf5aa7422e6d400c20ba
unknown <Admin@SMILE017.com.uct.ac.za>
2014-07-29 19:28:01 +0200

    Added boostrap to code base

--- application/modules/login/views/loginview.php
+++ application/modules/login/views/loginview.php
@@ -1,1 +1,17 @@
-<h1>Login View</h1>
+<html>
+    <head>
+        <title>Flexi Dash | Login</title>
+        <meta name="description" content="This is the login page"/>
+        <meta name="keywords" content="keywords anothre fdsfe "/>
+        
+        <!-- jQuery 2.0.2 -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+        <!-- bootstrap 3.0.2 -->
+        <link href="<?php echo base_url().'assets/css/bootstrap.min.css';?>" rel="stylesheet" type="text/css" />
+        <!-- Bootstrap -->
+        <script src="<?php echo base_url().'assets/js/bootstrap.min.js'; ?>" type="text/javascript"></script>
+    </head>
+    <body>
+        <h1>Login View</h1>
+    </body>
+</html>
